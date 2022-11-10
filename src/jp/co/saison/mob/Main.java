9bf1b83fa923519559eaee07c5aba1e382e0beb3
Yuya-Torii <Yuya_Torii@hulft.com>
2022-11-10 19:02:21 +0900

    torii 1st

--- src/jp/co/saison/mob/Main.java
+++ src/jp/co/saison/mob/Main.java
@@ -1,21 +1,27 @@
 package jp.co.saison.mob;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        File file = new File("../../../../../resource/weather.dat");
+        Path path = Paths.get("weather.dat");
+        File file = new File("");
         try {
             FileReader fr = new FileReader(file);
             BufferedReader br = new BufferedReader(fr);
-            String text = br.readLine();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            String text;
+            while ((text = br.readLine()) != null) {
+                System.out.println(text);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 }
