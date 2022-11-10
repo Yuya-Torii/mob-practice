4c4d4dc975da8b063301fd17b3b3c2d11199eb31
Shoya-Tobita <shoya_tobita@hulft.com>
2022-11-10 19:12:35 +0900

    リストの追加

--- src/jp/co/saison/mob/Main.java
+++ src/jp/co/saison/mob/Main.java
@@ -6,13 +6,14 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        Path path = Paths.get("weather.dat");
-        File file = new File("");
+        File file = new File("resource/weather.dat");
+        ArrayList<String> day = new ArrayList<>();
         try {
             FileReader fr = new FileReader(file);
             BufferedReader br = new BufferedReader(fr);
