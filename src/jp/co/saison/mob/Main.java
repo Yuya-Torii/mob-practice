cbe5fb826a681238ce8b884ce5d9a2ae2f26e695
Akihiko-Machii <Akihiko_Machii@sis2.saison.co.jp>
2022-11-10 18:42:18 +0900

    ファイルインポート

--- src/jp/co/saison/mob/Main.java
+++ src/jp/co/saison/mob/Main.java
@@ -1,8 +1,12 @@
 package jp.co.saison.mob;
 
+import java.io.File;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        File file = new File("../../../../../resource/weather.dat");
+
     }
 }
