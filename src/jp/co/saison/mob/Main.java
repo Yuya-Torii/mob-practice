9b45bcadb7db2d38866266567af677ee0ee23a07
Taisei_Ishii <Taisei_Ishii@hulft.com>
2022-11-10 18:52:37 +0900

    ishii 進捗

--- src/jp/co/saison/mob/Main.java
+++ src/jp/co/saison/mob/Main.java
@@ -1,12 +1,21 @@
 package jp.co.saison.mob;
 
-import java.io.File;
+import java.io.*;
 
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
         File file = new File("../../../../../resource/weather.dat");
+        try {
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+            String text = br.readLine();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
