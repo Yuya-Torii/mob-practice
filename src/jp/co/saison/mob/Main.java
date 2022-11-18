0c2f1b72b90a29489152b4c5e80083fb3d52928c
Shoya-Tobita <shoya_tobita@hulft.com>
2022-11-18 18:41:28 +0900

    mxtを切り取る処理の追加

--- src/jp/co/saison/mob/Main.java
+++ src/jp/co/saison/mob/Main.java
@@ -7,23 +7,25 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
         File file = new File("resource/weather.dat");
-        ArrayList<String> day = new ArrayList<>();
         try {
             FileReader fr = new FileReader(file);
             BufferedReader br = new BufferedReader(fr);
             String text;
             WeatherRecord weatherRecord = new WeatherRecord();
+            final List<WeatherRecord> list = new ArrayList<>();
             while ((text = br.readLine()) != null) {
                 if ("".equals(text)) {
                     continue;
                 }
                 String dy = text.substring(0, 4);
+                String mxt = text.substring(4,8);
                 System.out.println(dy);
             }
         } catch (IOException e) {
