2ae736a3c5001ad2b3dcc6bbd92f363fbc6d467e
Shunsuke-Nomura <shunsuke_nomura@hulft.com>
2022-12-02 18:59:31 +0900

    Part two の要件の実装をひとまず満たした

---
+++ src/jp/co/saison/mob/FootballRecord.java
@@ -0,0 +1,23 @@
+package jp.co.saison.mob;
+
+public class FootballRecord {
+
+    private final String name;
+    private final int forGoal;
+    private final int againstGoal;
+
+    public FootballRecord(String name, String forGoal, String againstGoal) {
+        this.name = name;
+        this.forGoal = Integer.parseInt(forGoal);
+        this.againstGoal = Integer.parseInt(againstGoal);
+    }
+
+    public int getGoalDiff() {
+        return Math.abs(forGoal - againstGoal);
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
