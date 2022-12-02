5cf78dbda5c2aa6eb0ef29251d8597989a4393fb
Kota_Matsumoto <kota_matsumoto@sis2.saison.co.jp>
2022-12-02 18:33:39 +0900

    BufferedReader 追加

--- src/jp/co/saison/mob/FootballMain.java
+++ src/jp/co/saison/mob/FootballMain.java
@@ -1,10 +1,17 @@
 package jp.co.saison.mob;
 
-import java.io.File;
+import java.io.*;
 
 public class FootballMain {
 	public static void main(String[] args) {
 		File file = new File("resource/football.dat");
+		try(final FileReader fileReader = new FileReader(file);
+			final BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+
 
 	}
 }
