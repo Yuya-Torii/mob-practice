f8c26ca86c5854d5d69e168e7fb4a47b6a965dd5
Maika_Onishi0528 <Maika_Onishi@hulft.com>
2023-01-13 19:46:52 +0900

    add football

--- src/jp/co/saison/mob/KataParserFactory.java
+++ src/jp/co/saison/mob/KataParserFactory.java
@@ -2,12 +2,15 @@ package jp.co.saison.mob;
 
 public class KataParserFactory {
 
-    public static KataParser createParser(String parserType) {
-        if (parserType.equals("weather")) {
-            return new WeatherParser();
-        } else {
-            return new FootballParser();
-        }
-    }
+	public static KataParser createParser(String parserType) {
+
+		if (parserType.equals("weather")) {
+			return new WeatherParser();
+		} else if (parserType.equals("football")) {
+			return new FootballParser();
+		} else {
+			throw new IllegalArgumentException();
+		}
+	}
 
 }
