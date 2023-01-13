4916891152a27996bf807ebc77e330989a90e7e2
yasuto_kato <yasuto_kato@sis2.saison.co.jp>
2023-01-13 19:35:08 +0900

    WeatherFormat の実装を追加

--- src/jp/co/saison/mob/Column.java
+++ src/jp/co/saison/mob/Column.java
@@ -2,13 +2,15 @@ package jp.co.saison.mob;
 
 public class Column {
 
+    private final String name;
     private final int startPosition;
 
     private final int length;
 
     private final ColumnType type;
 
-    public Column(int startPosition, int length, ColumnType type) {
+    public Column(String name, int startPosition, int length, ColumnType type) {
+        this.name = name;
         this.startPosition = startPosition;
         this.length = length;
         this.type = type;
--- src/jp/co/saison/mob/WeatherFormat.java
+++ src/jp/co/saison/mob/WeatherFormat.java
@@ -1,13 +1,18 @@
 package jp.co.saison.mob;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class WeatherFormat {
     // ヘッダーの行数
     private final int headerCount = 2;
     // レコード
     private final Record record;
 
-    public WeatherFormat(Record record) {
-        this.record = record;
+    public WeatherFormat() {
+        Set<Column> columns = new HashSet<>();
+        columns.add(new Column("Dy", 0, 4, ColumnType.Number));
+        this.record = new Record(columns);
     }
     // フッター
 
