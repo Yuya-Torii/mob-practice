fa8e1ad57e4041912a2e4e9b7b1b8c62ab41198f
Hironori Kuwahara <Hironori_Kuwahara@hulft.com>
2023-01-13 19:09:26 +0900

    Create FootballFormat.

--- src/jp/co/saison/mob/FootballParser.java
+++ src/jp/co/saison/mob/FootballParser.java
@@ -1,14 +1,16 @@
 package jp.co.saison.mob;
 
+import static jp.co.saison.mob.FootballFormat.*;
+
 public class FootballParser implements KataParser {
     @Override
     public KataRecord setRecord(String text) {
-        String name = text.substring(7, 21).replace(" ", "");
+        String name = text.substring(NAME_START, NAME_END).replace(" ", "");
         if (name.contains("--") || text.contains("Team")) {
             return null;
         }
-        String forGoal = text.substring(43, 45);
-        String againstGoal = text.substring(50, 52);
+        String forGoal = text.substring(FOR_GOAL_START, FOR_GOAL_END);
+        String againstGoal = text.substring(AGAINST_GOAL_START, AGAINST_GOAL_END);
 
         FootballRecord footballRecord = new FootballRecord(name, forGoal, againstGoal);
         return footballRecord;
