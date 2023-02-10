0f607e3a4eecc622e91a79d0f9dbba0772374e62
SISCO\Kengo_Minamide <kengo_minamide@hulft.com>
2023-02-10 19:01:28 +0900

    Listにいれた

--- src/jp/co/saison/mob/Kata04MainNew.java
+++ src/jp/co/saison/mob/Kata04MainNew.java
@@ -6,6 +6,7 @@ import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class Kata04MainNew {
@@ -17,15 +18,15 @@ public class Kata04MainNew {
         try (final FileReader fileReader = new FileReader(file);
              final BufferedReader bufferedReader = new BufferedReader(fileReader)) {
             String text;
-            ArrayList<KataRecord> kataRecords = new ArrayList<>();
+            List<Record> records = new ArrayList<>();
 
             // ColumnのSetをつくる
-	        Set<Column> columnSet = new HashSet<>();
-	        final Column dy = new Column("Dy", 0, 4, ColumnType.Number);
-			columnSet.add(dy);
-	        final Column mxt = new Column("MxT", 4, 6, ColumnType.Number);
-			columnSet.add(mxt);
-	        final Column mnt = new Column("MnT", 10, 6, ColumnType.Number);
+            Set<Column> columnSet = new HashSet<>();
+            final Column dy = new Column("Dy", 0, 4, ColumnType.Number);
+            columnSet.add(dy);
+            final Column mxt = new Column("MxT", 4, 6, ColumnType.Number);
+            columnSet.add(mxt);
+            final Column mnt = new Column("MnT", 10, 6, ColumnType.Number);
             columnSet.add(mnt);
 
             // Formatをつくる
@@ -34,16 +35,15 @@ public class Kata04MainNew {
             // Parserをつくる
             final Parser parser = new Parser(format);
 
-
             while ((text = bufferedReader.readLine()) != null) {
-                final Record record = parser.parse(text);
+                records.add(parser.parse(text));
             }
 
-            KataRecord minRecord = kataRecords.stream()
-                    .min(Comparator.comparing(KataRecord::getDiff))
-                    .orElseThrow(NullPointerException::new);
+//            KataRecord minRecord = kataRecords.stream()
+//                    .min(Comparator.comparing(KataRecord::getDiff))
+//                    .orElseThrow(NullPointerException::new);
 
-			System.out.println(minRecord.getDisplayName());
+ //           System.out.println(minRecord.getDisplayName());
 
 //            String result = parser.getRecord(filename);
         } catch (Exception e) {
