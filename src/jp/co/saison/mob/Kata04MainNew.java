9eb5019ad4d1960824d8f14148cec8755b625472
Yuya-Torii <Yuya_Torii@hulft.com>
2023-02-10 20:03:59 +0900

    TODOコメント追記

--- src/jp/co/saison/mob/Kata04MainNew.java
+++ src/jp/co/saison/mob/Kata04MainNew.java
@@ -29,6 +29,9 @@ public class Kata04MainNew {
             // Parserをつくる
             final Parser parser = new Parser(format);
 
+            // TODO 空行をどうスキップするか （ヘッダとか、非データ行の定義、情報をどう扱うか）
+            // TODO *付きなど、特殊なルールをどう扱うか
+            // TODO 小数点は処理できてる？
             String text;
             List<Record> records = new ArrayList<>();
             while ((text = bufferedReader.readLine()) != null) {
@@ -39,6 +42,9 @@ public class Kata04MainNew {
                 }
             }
 
+            // TODO minの中が冗長すぎる。短くしたい
+            // TODO キャストのやり方は大丈夫か、チェックしなくていい？
+            // TODO orElseThrow(NullPointerException いいの？
             final Record minRecord = records.stream()
                     .min(Comparator.comparing(r -> ((BigDecimal) r.items.get("MxT")).subtract(((BigDecimal) r.items.get("MnT")))))
                     .orElseThrow(NullPointerException::new);
@@ -52,6 +58,7 @@ public class Kata04MainNew {
 
             //           System.out.println(minRecord.getDisplayName());
 
+            // TODO WeatherとFootballの切り替えってどうやるの？
 //            String result = parser.getRecord(filename);
         } catch (Exception e) {
             e.printStackTrace();
